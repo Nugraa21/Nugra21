55f8bf2656614335f3da77453033dc081cd90e82
Nugraa21 <ludang400060@gmail.com>
2025-06-04 07:39:31 +0700

    -

--- src/data/data.js
+++ src/data/data.js
@@ -5,34 +5,34 @@ export const experienceData = [
     place: "Universitas Teknologi Digital Indonesia (UTDI)",
     time: "2022 - Sekarang",
     description:
-      "Mempelajari pemrograman, sistem digital, robotika, jaringan, dan Internet of Things (IoT).",
+      "Mempelajari dasar-dasar pemrograman, sistem digital, robotika, jaringan komputer, serta pengembangan perangkat berbasis Internet of Things (IoT).",
     delay: 300,
   },
   {
     icon: "Activity",
-    title: "IOT",
-    place: "Pembuatan perangkat IOT",
+    title: "Internet of Things (IoT)",
+    place: "Proyek Mandiri",
     time: "2024 - Sekarang",
     description:
-      "Membuat sebuah inovasi alat IOT dengan esp32 dan berberapa sensor dan mengembangkan dashboard monitoring nya",
+      "Mengembangkan perangkat IoT berbasis ESP32 dengan berbagai sensor, serta membangun dashboard monitoring interaktif menggunakan teknologi web & Aplikasi.",
     delay: 100,
   },
   {
     icon: "Activity",
     title: "Freelance Designer",
-    place: "Remote",
+        place: "Freelance (Fiverr,Instagram)",
     time: "2016 - Sekarang",
     description:
-      "Mengerjakan berbagai proyek desain grafis, video editing, dan UI/UX untuk klien lokal dan luar negeri.",
+      "Mengerjakan project editing vide (Intro) dan design web interaktif.",
     delay: 500,
   },
   {
     icon: "Activity",
     title: "Frontend Developer",
-    place: "Fiver",
+    place: "Freelance (Fiverr,Instagram)",
     time: "2024 - Sekarang",
     description:
-      "Merancang antarmuka pengguna interaktif menggunakan Next.js dan Tailwind CSS untuk aplikasi.",
+      "Merancang dan mengembangkan antarmuka web modern menggunakan Next.js dan Tailwind CSS, fokus pada performa, responsivitas, dan pengalaman pengguna.",
     delay: 700,
   },
 ];
