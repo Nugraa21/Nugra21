fdbeb0455bc33a858233ef4d01f48b87ba27ea52
Nugraa21 <ludang400060@gmail.com>
2025-06-13 20:27:16 +0700

    -

--- src/components/Certificate.jsx
+++ src/components/Certificate.jsx
@@ -6,7 +6,7 @@ import LinkIcon from "@mui/icons-material/Link";
 
 const Certificate = ({ ImgSertif, title, description, issuer, date, Link }) => {
   const [open, setOpen] = useState(false);
-
+ 
   const handleOpen = () => {  
     setOpen(true);  
   };  
